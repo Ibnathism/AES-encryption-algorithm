cddb46d39cbcf2c482047279e6f2e247fc4be034
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-03-10 02:47:42 +0600

    decrypt

--- AES_encryption.py
+++ AES_encryption.py
@@ -18,21 +18,43 @@ Sbox = [
     [0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16],
 ]
 
-def read_sbox(index_string):
+InvSbox = (
+    [0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB],
+    [0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB],
+    [0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E],
+    [0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25],
+    [0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92],
+    [0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84],
+    [0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06],
+    [0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B],
+    [0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73],
+    [0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E],
+    [0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B],
+    [0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4],
+    [0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F],
+    [0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF],
+    [0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61],
+    [0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D],
+)
+
+def read_sbox(index_string, is_inverse):
     row = int(index_string[0], 16)
     col = int(index_string[1], 16)
     #print(Sbox[row][col])
-    temp = Sbox[row][col]
+    if is_inverse:
+        temp = InvSbox[row][col]
+    else:
+        temp = Sbox[row][col]
     temp_bv = BitVector(intVal=temp, size=16)
     s_box_data = temp_bv.get_bitvector_in_hex()
     #print(s_box_data+" when index string "+index_string)
     return str(s_box_data)[2:4]
 
-def generate_sub_bytes(word):
+def generate_sub_bytes(word, is_inverse):
     length = len(word)
     sub_bytes = ""
     for i in range(0, length-1, 2):
-        temp = read_sbox(word[i:i+2])
+        temp = read_sbox(word[i:i+2], is_inverse)
         sub_bytes = sub_bytes + temp
     return sub_bytes
 
@@ -51,10 +73,17 @@ def cyclic_shift_row(state_mat):
 
     return w0 + w1 + w2 + w3 
 
+def inverse_cyclic_shift_row(state_mat):
+    w0 = state_mat[0:2] + state_mat[26:28] + state_mat[20:22] + state_mat[14:16]
+    w1 = state_mat[8:10] + state_mat[2:4] + state_mat[28:30] + state_mat[22:24]
+    w2 = state_mat[16:18] + state_mat[10:12] + state_mat[4:6] + state_mat[30:32]
+    w3 = state_mat[24:26] + state_mat[18:20] + state_mat[12:14] + state_mat[6:8]
+
+    return w0 + w1 + w2 + w3 
 
 def g(word, round_constant):
     row_shifted_word = shift_row(word)
-    sub_bytes = generate_sub_bytes(row_shifted_word)
+    sub_bytes = generate_sub_bytes(row_shifted_word, False)
     result = add_round_key(sub_bytes, round_constant)
     return result
 
@@ -79,8 +108,11 @@ def get_linear_val(row, col, str):
     idx = row*2 + col*8
     return str[idx:idx+2]
 
-def get_mix_column_value(row, col, state_mat):
-    mix_con = "02010103030201010103020101010302"
+def get_mix_column_value(row, col, state_mat, is_inverse):
+    if is_inverse:
+        mix_con = "0E090D0B0B0E090D0D0B0E09090D0B0E"
+    else:
+        mix_con = "02010103030201010103020101010302"
     m0 = multiply(get_linear_val(row, 0, mix_con), get_linear_val(0, col, state_mat))
     m1 = multiply(get_linear_val(row, 1, mix_con), get_linear_val(1, col, state_mat))
     m2 = multiply(get_linear_val(row, 2, mix_con), get_linear_val(2, col, state_mat))
@@ -90,11 +122,11 @@ def get_mix_column_value(row, col, state_mat):
     temp = add_round_key(temp, m3)
     return temp
 
-def apply_mix_column(state_mat):
+def apply_mix_column(state_mat, is_inverse):
     temp = ''
     for col in range(0,4):
         for row in range(0,4):
-            temp = temp + get_mix_column_value(row, col, state_mat)
+            temp = temp + get_mix_column_value(row, col, state_mat, is_inverse)
     return temp
 
 def get_rk_list(keyInHex):
@@ -122,17 +154,25 @@ def encrypt(rkList, textInHex):
     state_mat = add_round_key(rkList[0], textInHex)
 
     for r in range(1, 11):
-        state_mat = generate_sub_bytes(state_mat)
+        state_mat = generate_sub_bytes(state_mat, False)
         state_mat = cyclic_shift_row(state_mat)
         if r != 10:
-            state_mat = apply_mix_column(state_mat)
+            state_mat = apply_mix_column(state_mat, False)
         state_mat = add_round_key(state_mat, rkList[r])
         #print(r, ': ', state_mat)
     return state_mat
 
+def decrypt(rkList, cipherText):
+    state_mat = add_round_key(rkList[10], cipherText)
 
-
-
+    for r in range(1, 11):
+        state_mat = inverse_cyclic_shift_row(state_mat)
+        state_mat = generate_sub_bytes(state_mat, True)
+        state_mat = add_round_key(state_mat, rkList[10-r])
+        if r != 10:
+            state_mat = apply_mix_column(state_mat, True)
+        print(r, ': ', state_mat)
+    return state_mat
 
 key = "Thats my Kung Fu"
 keyInHex = BitVector(textstring=key).get_bitvector_in_hex()
@@ -145,3 +185,7 @@ rk_list = get_rk_list(keyInHex)
 cipherText = encrypt(rk_list, textInHex)
 print(cipherText)
 
+result = decrypt(rk_list, cipherText)
+print(result)
+result = BitVector(hexstring=result)
+print(result.get_bitvector_in_ascii())
