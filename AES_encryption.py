76d42dbfee5c0715804b5be25bee12b493c52970
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-03-13 12:04:04 +0600

    read and write image

--- AES_encryption.py
+++ AES_encryption.py
@@ -1,6 +1,6 @@
 from BitVector import *
 from bitstring import ConstBitStream
-
+import base64 
 Sbox = [
     [0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76],
     [0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0],
@@ -262,12 +262,37 @@ def decrypt(encrypted_text, file_name):
     f = open(file_name, 'a')
     f.write(den)
 
+def r_w_image():
+    with open("input.png", "rb") as image2string: 
+        text = base64.b64encode(image2string.read()) 
+    #print(converted_string) 
+    text = text.decode('utf-8')
+    l = len(text)
+    encrypted_text = ''
+    for i in range(0, l-1, 16):
+        t = text[i:i+16]
+        if i+16 > l:
+            t = t.ljust(16)
+        temp = encrypt_text(rks, t)
+        print('Cipher', temp)
+        encrypted_text = encrypted_text + temp
+
+    l = len(encrypted_text)
+    den = ''
+    for i in range(0, l-1, 32):
+        temp = decrypt_text(rks, encrypted_text[i:i+32])
+        den = den + temp
+    decodeit = open('output.png', 'wb') 
+    decodeit.write(base64.b64decode((den)))
+
 key = "Thats my Kung Fuhgjhghg"
 
 rks = init(key)
 
-en_text = encrypt("input.txt")
+# en_text = encrypt("input.txt")
 
-decrypt(en_text, "output.txt")
+# decrypt(en_text, "output.txt")
 
 
+
+r_w_image()
